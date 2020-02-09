4c64a69d9ed1650886916bf9b2ed8a6ba736fde3
Patrick A <akolo@hotmail.com>
2020-02-09 09:04:59 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -20,11 +20,12 @@ def find_greater_pair(src)
 end
 
 def total_even_pairs(src)
-  total = 0
+  array_3 = 0
   row_index = 0 
   while row_index < src.count do
-    if (src[row_index][0] %2 == 0) &&(src[row_index][1] %2 == 0)
-    total =+ (src[row_index][0] + src[row_index][1]) 
+    
+    if (src[row_index][0] %2 == 0) && (src[row_index][1] %2 == 0)
+      total =+ (src[row_index][0] + src[row_index][1]) 
     end
     row_index += 1 
   end 
